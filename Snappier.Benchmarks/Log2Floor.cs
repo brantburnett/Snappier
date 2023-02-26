ed80ce06cf5c1c4ef798bc196eb1c8cec5fe9916
Brant Burnett <bburnett@centeredgesoftware.com>
2023-02-26 11:02:12 -0500

    Improve Log2Floor for legacy runtimes
    Motivation
    ----------
    There are more performant algorithms for Log2Floor available for legacy
    runtimes where BitOperations and intrinsics are not available.
    
    Modifications
    -------------
    Port the fallback used by BitOperations.Log2 when hardware acceleration
    is not available.
    
    Results
    -------
    BenchmarkDotNet=v0.13.4, OS=Windows 11 (10.0.22000.1641/21H2)
    Intel Core i7-10850H CPU 2.70GHz, 1 CPU, 12 logical and 6 physical cores
    .NET SDK=7.0.200
      [Host]     : .NET 7.0.3 (7.0.323.6910), X64 RyuJIT AVX2
      Job-DYBDUA : .NET Framework 4.8 (4.8.4614.0), X64 RyuJIT VectorSize=256
      Job-UUNMHF : .NET Framework 4.8 (4.8.4614.0), X64 RyuJIT VectorSize=256
    
    EnvironmentVariables=Empty  Runtime=.NET Framework 4.8
    
    |    Method | BuildConfiguration |      Mean |     Error |    StdDev | Ratio | Rank | Code Size |
    |---------- |------------------- |----------:|----------:|----------:|------:|-----:|----------:|
    | Log2Floor |           Previous | 18.661 ns | 0.0988 ns | 0.0876 ns |  1.00 |    2 |     282 B |
    | Log2Floor |            Default |  9.521 ns | 0.0515 ns | 0.0402 ns |  0.51 |    1 |     180 B |
    
    This should consequently provide a small boost to compression speed on
    legacy frameworks, primarily when writing large literals > 60 bytes.

---
+++ Snappier.Benchmarks/Log2Floor.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using Snappier.Internal;
+
+namespace Snappier.Benchmarks
+{
+    [DisassemblyDiagnoser(2)]
+    public class Log2FloorHelper
+    {
+        private uint _n = 5;
+
+        [Benchmark]
+        public int Log2Floor()
+        {
+            return Helpers.Log2Floor(_n);
+        }
+    }
+}
