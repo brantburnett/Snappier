18dc324746e66fa880a04cf7cc5bf761cc6f4e82
Brant Burnett <bburnett@centeredgesoftware.com>
2024-12-15 09:08:29 -0500

    Improve the efficiency of decompressing to IBufferWriter<byte>

---
+++ Snappier/Internal/IsExternalInit.cs
@@ -0,0 +1,15 @@
+﻿#if NETSTANDARD2_0
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#pragma warning disable IDE0079
+#pragma warning disable S3903
+
+namespace System.Runtime.CompilerServices;
+
+internal static class IsExternalInit
+{
+}
+
+#endif
